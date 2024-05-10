1de0d01da1790b2ce0a25b4ae7918fe1d5b521e0
RyBry <rion257@outlook.com>
2024-05-10 12:45:12 -0700

    Final Challenge: added animated, clickable albums with hyperlinks and useState

--- components/custom/Showcase/Images.js
+++ components/custom/Showcase/Images.js
@@ -19,15 +19,17 @@ export default function ShowcaseImages({ activeIndex, isExpanded, items }) {
                     transform: `translateX(-${(activeIndex / items.length) * 100}%)`
                 }}>
                 {items.map((item, index) => {
-                    return <li className={styles.showcase__images__list__item} key={index}>
-                        <Image
-                            src={item.images[0].url}
-                            alt={item.name}
-                            width={item.images[0].width}
-                            height={item.images[0].height}
-                            className={styles.showcase__images__list__item__image}
-                        />
-                    </li>
+                    return (
+                        <li className={styles.showcase__images__list__item} key={index}>
+                            <Image
+                                src={item.images[0].url}
+                                alt={item.name}
+                                width={item.images[0].width}
+                                height={item.images[0].height}
+                                className={styles.showcase__images__list__item__image}
+                            />
+                        </li>
+                    );
                 })}
             </ul>
         </div>
--- components/layout/Grid/index.js
+++ components/layout/Grid/index.js
@@ -3,8 +3,10 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import styles from "./Grid.module.scss";
 import Paragraph from "../../html/Paragraph";
+import { useState, useEffect } from "react";
 
 export default function Grid({ items }) {
+
 
     const listVariants = {
         initial: {
@@ -26,8 +28,9 @@ export default function Grid({ items }) {
         },
         animate: {
             opacity: 1,
-            y: 0
-        }
+            y: 0,
+        },
+
     }
 
     return (
@@ -38,19 +41,43 @@ export default function Grid({ items }) {
             animate="animate"
         >
             {items.map((item, index) => {
+                const [mouseOverImage, setMouseOverImage] = useState(false);
+
                 return (
+
                     <motion.li
                         className={styles.grid__item}
                         key={`album-${index}`}
                         variants={listItemVariants}
                     >
-                        <Image
-                            src={item.images[0].url}
-                            alt={item.name}
-                            width={item.images[0].width}
-                            height={item.images[0].height}
-                            className="responsive-img"
-                        />
+                        <a
+                            style={{
+                                paddingLeft: mouseOverImage ? "20px" : "0px",
+                                paddingRight: mouseOverImage ? "20px" : "0px",
+                                cursor: "pointer",
+                                transition: "all 0.2s",
+                                opacity: mouseOverImage ? "0.5" : "1"
+                            }}
+                            onMouseOver={() => {
+                                console.log("ugh");
+                                setMouseOverImage(true);
+                            }}
+                            onMouseOut={() => {
+                                setMouseOverImage(false)
+                            }}
+                            onMouseDown={() => {
+                                window.open(item.external_urls.spotify, "_blank").focus();
+                            }}
+                        >
+                            <Image
+                                src={item.images[0].url}
+                                alt={item.name}
+                                width={item.images[0].width}
+                                height={item.images[0].height}
+                                className="responsive-img"
+                            />
+                        </a>
+
                         <Paragraph>{item.name}</Paragraph>
                     </motion.li>
                 );
